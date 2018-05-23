2d0e5a7d96bbb050546882b382c74fb2f0b9fdd2
TerryHannant <thannant_github@metafu.net>
2018-05-23 11:22:34 +0200

    Add get by identifier

--- src/main/java/no/nmdc/oaipmh/provider/service/MetadataService.java
+++ src/main/java/no/nmdc/oaipmh/provider/service/MetadataService.java
@@ -2,7 +2,9 @@ package no.nmdc.oaipmh.provider.service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 import javax.xml.bind.JAXBException;
+import no.nmdc.oaipmh.provider.dao.dto.Dataset;
 import no.nmdc.oaipmh.provider.domain.OAIPMHtype;
 import no.nmdc.oaipmh.provider.domain.dif.DIF;
 
@@ -34,4 +36,11 @@ public interface MetadataService {
      * @return 
      */
     List<DIF> getDifRecords(String set);
+    
+
+    DIF getDifRecord(String identifier);
+    
+    
+    
+   
 }
